54788e026ea0ab6e1daaa64f8af005053e9a077b
Mtsky <95162192+Gaoc3@users.noreply.github.com>
2024-06-04 20:33:15 +0300

    Update TTs.py

--- Mtsky-AI/TTs.py
+++ Mtsky-AI/TTs.py
@@ -19,27 +19,3 @@ def audioTTs(prompet: str, rate: str = '+0%', volume: str = '+100%', value: str 
 # print(response.json())
 # with open('simple.mp3','wb') as file:
 #     file.write(b64decode(response.json()['openai']['audio']))
-import requests
-
-headers = {
-    'Authorization': 'Bearer <ACCESS TOKEN>',
-    'Content-Type': 'application/json',
-}
-
-json_data = {
-    'platform': 'edge_tts',
-    'message': '欢迎回家',
-    'language': 'zh-CN-XiaoyiNeural',
-    'cache': True,
-    'options': {
-        'volume': '+10%',
-    },
-}
-
-response = requests.post('http://home-assistant.local:8123/api/tts_get_url', headers=headers, json=json_data)
-print(response.json())
-
-# Note: json_data will not be serialized by requests
-# exactly as it was in the original request.
-#data = '{"platform": "edge_tts", "message": "欢迎回家", "language": "zh-CN-XiaoyiNeural", "cache": true, "options": {"volume": "+10%"}}'.encode()
-#response = requests.post('http://home-assistant.local:8123/api/tts_get_url', headers=headers, data=data)
